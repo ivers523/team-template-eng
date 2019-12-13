ddf9dc9b864744d2d7b7253374d9d7affdbaf676
ivers523 <ivers523@umn.edu>
2019-12-12 19:25:13 -0600

    implemented role selection logic

--- Develop/app.js
+++ Develop/app.js
@@ -8,28 +8,52 @@ const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 
 // prompt user for information
-function startPrompt(){
+function startPrompt() {
   inquirer.prompt(
     {
-        type: "list",
-        message: "Would you like to create a team?",
-        choices: ['Yes', 'No'],
-        name: "start"
-      }
+      type: "list",
+      message: "Would you like to create a team?",
+      choices: ['Yes', 'No'],
+      name: "start"
+    }
   )
-  .then(function(res){
-    if(res.start === 'Yes')
-    console.log("build team")
-    buildTeam();
-  })
+    .then(function (res) {
+      if (res.start === 'Yes')
+        console.log("build team")
+      buildTeam();
+    })
+  // add "else" behavior here
 }
-
 // create function to ask about team (build team)
-
-
-
-
-
-
-
+// get core info as data
+function buildTeam() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter employee e-mail address",
+      name: "email"
+    },
+    {
+      type: "input",
+      message: "Enter employee ID number",
+      name: "id"
+    },
+    {
+      type: "list",
+      message: "Finally, provide one of the following:",
+      choices: ['School', 'Github', 'Office Number'],
+      name: "role"
+    }
+  ])
+  .then (function(res){
+    if (res.role==='School'){
+      console.log("Intern");
+    } else if (res.role==='Github'){
+      console.log("Engineer");
+    } else {
+      console.log("Manager")
+    }
+  }
+  )
+}  
 startPrompt();
