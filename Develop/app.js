e90a0ccbbe55041237d7316146d13aa4abdcf6fa
ivers523 <ivers523@umn.edu>
2019-12-12 18:50:02 -0600

    start prompt functioning

--- Develop/app.js
+++ Develop/app.js
@@ -1,2 +1,35 @@
-// set dependencies (inquirer, gist)
-// prompt user for information(email, ID, role)
+// import packages
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const Employee = require("./lib/employee");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+
+// prompt user for information
+function startPrompt(){
+  inquirer.prompt(
+    {
+        type: "list",
+        message: "Would you like to create a team?",
+        choices: ['Yes', 'No'],
+        name: "start"
+      }
+  )
+  .then(function(res){
+    if(res.start === 'Yes')
+    console.log("build team")
+    buildTeam();
+  })
+}
+
+// create function to ask about team (build team)
+
+
+
+
+
+
+
+startPrompt();
