f8385a6b981b28678c29301c35d86224014d1681
ivers523 <ivers523@umn.edu>
2019-12-12 22:16:40 -0600

    all rolls build objects

--- Develop/app.js
+++ Develop/app.js
@@ -42,7 +42,7 @@ function findRole() {
         case 'School':
           buildIntern(res);
           break;
-        case 'Github Profile':
+        case 'Github Username':
           buildEngineer(res);
           break;
         case 'Office Number':
@@ -51,7 +51,7 @@ function findRole() {
       }
     })
 }
-// 
+// build roles
 function buildIntern(res) {
   let intern = new Intern(res.school, res.name, res.id, res.email);
   inquirer.prompt([
@@ -80,8 +80,74 @@ function buildIntern(res) {
       intern.school = res.school;
       intern.name = res.name;
       intern.id = res.id;
-      intern.email=res.email;
+      intern.email = res.email;
       team.push(intern);
+      console.log(team);
+    })
+}
+function buildEngineer(res) {
+  let engineer = new Engineer(res.github, res.name, res.id, res.email);
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter Github username:",
+      name: "github"
+    },
+    {
+      type: "input",
+      message: "Enter Engineer Name:",
+      name: "name"
+    },
+    {
+      type: "input",
+      message: "Enter ID:",
+      name: "id"
+    },
+    {
+      type: "input",
+      message: "Enter email addres:",
+      name: "email"
+    },
+  ])
+    .then(function (res) {
+      engineer.github = res.github;
+      engineer.name = res.name;
+      engineer.id = res.id;
+      engineer.email = res.email;
+      team.push(engineer);
+      console.log(team);
+    })
+}
+function buildManager(res) {
+  let manager = new Manager(res.officenumber, res.name, res.id, res.email);
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter office number:",
+      name: "officenumber"
+    },
+    {
+      type: "input",
+      message: "Enter Manager Name:",
+      name: "name"
+    },
+    {
+      type: "input",
+      message: "Enter ID:",
+      name: "id"
+    },
+    {
+      type: "input",
+      message: "Enter email addres:",
+      name: "email"
+    },
+  ])
+    .then(function (res) {
+      manager.officenumber = res.officenumber;
+      manager.name = res.name;
+      manager.id = res.id;
+      manager.email = res.email;
+      team.push(manager);
       console.log(team);
     })
 }
