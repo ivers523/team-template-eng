38f88a016ec425cf5bf7d59e0740b358984ffb93
ivers523 <ivers523@umn.edu>
2019-12-15 21:03:19 -0600

    fixed scope errors

--- Develop/app.js
+++ Develop/app.js
@@ -7,8 +7,10 @@ const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 
-const team = [];
-let teamLength;
+// empty array of team members
+const teamArray = [];
+const teamLength = [];
+
 
 // prompt user for information
 function startPrompt() {
@@ -20,177 +22,181 @@ function startPrompt() {
       name: "start"
     }
   )
-  .then(function (res) {
-      if (res.start === 'Yes'){
+    .then(function (res) {
+      if (res.start === 'Yes') {
         console.log("build team");
         teamSize();
-    } else {
-      console.log("insert else functionality");
-    }
-})
-
-// determine number of team members
-function teamSize() {
-  inquirer.prompt(
-    {
-      type: "list",
-      message: "Please select team size",
-      choices: [1, 2, 3, 4, 5, 6],
-      name: "teamsize"
-    })
-    .then(function (res) {
-      console.log(res.teamsize);
-      findRole();
-      // NEED TO FIND WAY TO SAVE TEAM LENGTH TO GLOBAL VARIABLE
+      } else {
+        console.log("insert else functionality");
       }
-      
+    })
+
+  // determine number of team members
+  function teamSize() {
+    inquirer.prompt(
+      {
+        type: "list",
+        message: "Please select team size",
+        choices: [1, 2, 3, 4, 5, 6],
+        name: "teamsize"
+      })
+      .then(function (res) {
+        teamLength.push(res);
+        console.log(teamLength);
+        findRole();
+        // NEED TO FIND WAY TO SAVE TEAM LENGTH TO GLOBAL VARIABLE
+      }
+
+      )
+  }
+
+  function findRole() {
+    inquirer.prompt(
+      {
+        type: "list",
+        message: "To add a new team member, please select one of the following:",
+        choices: ['School', 'Github Username', 'Office Number'],
+        name: "role"
+      }
+      // this was done with the intent of not asking their direct role, but assigning their class based on their input
     )
-}
-function findRole() {
-  inquirer.prompt(
-    {
-      type: "list",
-      message: "To add a new team member, please select one of the following:",
-      choices: ['School', 'Github Username', 'Office Number'],
-      name: "role"
-    }
-  )
-    .then(function (res) {
-      switch (res.role) {
-        case 'School':
-          buildIntern(res);
-          break;
-        case 'Github Username':
-          buildEngineer(res);
-          break;
-        case 'Office Number':
-          buildManager(res);
-          break;
-      }
-    })
-}
-// build roles
-function buildIntern(res){
-  let intern = new Intern(res.school, res.name, res.id, res.email);
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "Enter Intern School:",
-      name: "school"
-    },
-    {
-      type: "input",
-      message: "Enter Intern Name:",
-      name: "name"
-    },
-    {
-      type: "input",
-      message: "Enter ID:",
-      name: "id"
-    },
-    {
-      type: "input",
-      message: "Enter email addres:",
-      name: "email"
-    }
-  ])
-    .then(function (res){
-    intern.school = res.school;
-    intern.name = res.name;
-    intern.id = res.id;
-    intern.email = res.email;
-    if (team.length < teamLength){
-      team.push(intern);
-      findRole();
-    } else {
-      writeToHTML();
-    }
-  
- })
-}
-}
-function buildEngineer(res){
-  let engineer = new Engineer(res.github, res.name, res.id, res.email);
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "Enter Engineer Github username:",
-      name: "github"
-    },
-    {
-      type: "input",
-      message: "Enter Engineer Name:",
-      name: "name"
-    },
-    {
-      type: "input",
-      message: "Enter ID:",
-      name: "id"
-    },
-    {
-      type: "input",
-      message: "Enter email addres:",
-      name: "email"
-    }
-  ])
-    .then(function (res){
-    engineer.github = res.github;
-    engineer.name = res.name;
-    engineer.id = res.id;
-    engineer.email = res.email;
-    if (team.length < teamLength){
-      team.push(engineer);
-      findRole();
-    } else {
-      writeToHTML();
-    }
-  
- })
-}
-function buildManager(res){
-  let manager = new Manager(res.officenumber, res.name, res.id, res.email);
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "Enter Manager office number:",
-      name: "officenumber"
-    },
-    {
-      type: "input",
-      message: "Enter Manager Name:",
-      name: "name"
-    },
-    {
-      type: "input",
-      message: "Enter ID:",
-      name: "id"
-    },
-    {
-      type: "input",
-      message: "Enter email addres:",
-      name: "email"
-    }
-  ])
-    .then(function (res){
-    manager.officenumber = res.officenumber;
-    manager.name = res.name;
-    manager.id = res.id;
-    manager.email = res.email;
-    if (team.length < teamLength){
-      team.push(manager);
-      findRole();
-    } else {
-      writeToHTML();
-    }
-  
- })
-}
+      .then(function (res) {
+        switch (res.role) {
+          case 'School':
+            buildIntern(res);
+            break;
+          case 'Github Username':
+            buildEngineer(res);
+            break;
+          case 'Office Number':
+            buildManager(res);
+            break;
 
-function writeToHTML(){
-  console.log("write to hmtl");
-  // fs.writeFile("./output/team.html", buildHTML())
-}
-startPrompt();
+        }
+      })
 
+    function buildIntern(res) {
+      let intern = new Intern(res.school, res.name, res.id, res.email);
+      inquirer.prompt([
+        {
+          type: "input",
+          message: "Enter Intern School:",
+          name: "school"
+        },
+        {
+          type: "input",
+          message: "Enter Intern Name:",
+          name: "name"
+        },
+        {
+          type: "input",
+          message: "Enter ID:",
+          name: "id"
+        },
+        {
+          type: "input",
+          message: "Enter email addres:",
+          name: "email"
+        }
+      ])
+        .then(function (res) {
+          intern.school = res.school;
+          intern.name = res.name;
+          intern.id = res.id;
+          intern.email = res.email;
+          teamArray.push(intern);
+          console.log(teamArray);
+          if (teamArray.length < 3) {
+            findRole();
+          } else {
+            writeToHTML();
+          }
 
+        })
+    }
+      function buildEngineer(res) {
+        let engineer = new Engineer(res.github, res.name, res.id, res.email);
+        inquirer.prompt([
+          {
+            type: "input",
+            message: "Enter Engineer Github username:",
+            name: "github"
+          },
+          {
+            type: "input",
+            message: "Enter Engineer Name:",
+            name: "name"
+          },
+          {
+            type: "input",
+            message: "Enter ID:",
+            name: "id"
+          },
+          {
+            type: "input",
+            message: "Enter email addres:",
+            name: "email"
+          }
+        ])
+          .then(function (res) {
+            engineer.github = res.github;
+            engineer.name = res.name;
+            engineer.id = res.id;
+            engineer.email = res.email;
+            teamArray.push(engineer);
+            console.log(teamArray);
+            if (teamArray.length < 3) {
+              findRole();
+            } else {
+              writeToHTML();
+            }
+          })
+        }
+            function buildManager(res) {
+              let manager = new Manager(res.officenumber, res.name, res.id, res.email);
+              inquirer.prompt([
+                {
+                  type: "input",
+                  message: "Enter Manager office number:",
+                  name: "officenumber"
+                },
+                {
+                  type: "input",
+                  message: "Enter Manager Name:",
+                  name: "name"
+                },
+                {
+                  type: "input",
+                  message: "Enter ID:",
+                  name: "id"
+                },
+                {
+                  type: "input",
+                  message: "Enter email addres:",
+                  name: "email"
+                }
+              ])
+                .then(function (res) {
+                  manager.officenumber = res.officenumber;
+                  manager.name = res.name;
+                  manager.id = res.id;
+                  manager.email = res.email;
+                  teamArray.push(manager);
+                  console.log(teamArray);
+                  if (teamArray.length < 3) {
+                    findRole();
+                  } else {
+                    writeToHTML();
+                  }
+
+                })
+            }
+          }
+        } 
+    // build roles
+    function writeToHTML() {
+      console.log("write to hmtl");
+      // fs.writeFile("./output/team.html", buildHTML())
+    }
+    startPrompt();
 
